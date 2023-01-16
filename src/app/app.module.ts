d7ba5431f5e7371d03800299670db66dd4e88fc2
Jim McPike <jmcpike@heltiechonliune.com>
2023-01-16 16:15:32 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent
   ],
   imports: [
     BrowserModule
